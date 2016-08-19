2a635488eb6d168fc15af5362e05f3fe71a636d5
Elias Annila <elias.annila@gmail.com>
2016-08-19 09:41:20 +0300

    ui done

---
+++ src/plugins/least_cost_path/leastcostpathgui.cpp
@@ -0,0 +1,103 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Tim Sutton                                      *
+ *   tim@linfiniti.com                                                     *
+ *                                                                         *
+ *   This is a plugin generated from the QGIS plugin template              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+#include "leastcostpathgui.h"
+#include "qgscontexthelp.h"
+#include "qgsmaplayerregistry.h"
+#include "qgsvectorlayer.h"
+#include <QSettings>
+#include <QDir>
+#include <QFileDialog>
+#include <QFileInfo>
+#include <iostream>
+
+//qt includes
+
+//standard includes
+
+LeastCostPathGui::LeastCostPathGui(QWidget* parent, Qt::WindowFlags fl) :
+		QDialog(parent, fl) {
+	setupUi(this);
+
+	//insert available vector layers
+	//enter available layers into the layer combo boxes
+	QMap<QString, QgsMapLayer*> mapLayers =
+			QgsMapLayerRegistry::instance()->mapLayers();
+	QMap<QString, QgsMapLayer*>::iterator layer_it = mapLayers.begin();
+
+	//insert available input layers
+	for (; layer_it != mapLayers.end(); ++layer_it) {
+		QgsVectorLayer* vl = qobject_cast<QgsVectorLayer *>(layer_it.value());
+		if (vl) {
+			mStartComboBox->addItem(vl->name(), QVariant(vl->id()));
+			mTargetComboBox->addItem(vl->name(), QVariant(vl->id()));
+			mCostSurfaceComboBox->addItem(vl->name(), QVariant(vl->id()));
+		}
+	}
+
+}
+
+void LeastCostPathGui::on_mOutputLayerToolButton_clicked() {
+	QSettings s;
+	QString lastDir =
+			s.value("/LeastCostPath/lastOutputDir", QDir::homePath()).toString();
+	QString saveFileName = QFileDialog::getSaveFileName(nullptr,
+			tr("Enter output file"), lastDir);
+	if (!saveFileName.isNull()) {
+		mOutputLayerLineEdit->setText(saveFileName);
+	}
+}
+
+LeastCostPathGui::~LeastCostPathGui() {
+}
+
+void LeastCostPathGui::on_buttonBox_accepted() {
+	accept();
+}
+
+QString LeastCostPathGui::outputFile()
+{
+  QString outputFileName = mOutputLayerLineEdit->text();
+  QFileInfo fileInfo( outputFileName );
+  QString suffix = fileInfo.suffix();
+  if ( !suffix.isEmpty() )
+  {
+    return outputFileName;
+  }
+
+  //add the file format extension if the user did not specify it (shp for now...)
+
+  return ( outputFileName + ".shp" );
+}
+
+
+QString LeastCostPathGui::costSurface()
+{
+  //AIHEUTTAA CRASHIN
+  QgsMapLayer* inputLayer = QgsMapLayerRegistry::instance()->mapLayer( mCostSurfaceComboBox->itemData( mCostSurfaceComboBox->currentIndex() ).toString() );
+   if ( !inputLayer )
+  {
+    return "";
+  }
+
+  QString inputFilePath = inputLayer->source();
+  return inputFilePath;
+}
+
+
+void LeastCostPathGui::on_buttonBox_rejected() {
+	reject();
+}
+
+void LeastCostPathGui::on_buttonBox_helpRequested() {
+	//QgsContextHelp::run( context_id );
+}
+
---
+++ src/plugins/least_cost_path/leastcostpathgui.h
@@ -0,0 +1,42 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Tim Sutton                                      *
+ *   tim@linfiniti.com                                                     *
+ *                                                                         *
+ *   This is a plugin generated from the QGIS plugin template              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+#ifndef LeastCostPathGUI_H
+#define LeastCostPathGUI_H
+
+#include <QDialog>
+#include <ui_leastcostpathguibase.h>
+
+/**
+@author Tim Sutton
+*/
+class LeastCostPathGui : public QDialog, private Ui::LeastCostPathGuiBase
+{
+    Q_OBJECT
+  public:
+    LeastCostPathGui( QWidget* parent = 0, Qt::WindowFlags fl = 0 );
+    QString outputFile();
+    QString costSurface();
+    ~LeastCostPathGui();
+
+  private:
+    static const int context_id = 0;
+
+  private slots:
+    void on_buttonBox_accepted();
+    void on_buttonBox_rejected();
+    void on_buttonBox_helpRequested();
+    void on_mOutputLayerToolButton_clicked();
+
+
+};
+
+#endif
